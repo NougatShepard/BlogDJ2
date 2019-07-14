d54e06721d5a3745920492b61a58c75987948eb9
NougatShepard <jdwchn@gmail.com>
2019-07-14 21:39:08 +0900

    Site updated: 2019-07-14 21:39:05

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Sun Jul 14 2019 19:43:33 GMT+0900 (GMT+09:00)
+// build time:Sun Jul 14 2019 21:38:53 GMT+0900 (GMT+09:00)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
